4cffc28ce1fd0ad0962f600d4336b19b6c21a018
levct <2303247590@qq.com>
2019-12-06 14:40:43 +0800

    新增热门组件图片

--- src/views/hot/Index.js
+++ src/views/hot/Index.js
@@ -1,50 +1,97 @@
-export const hotList = [
-    {
-        id :'1',
-        title:'纸巾湿品',
-        imgPath:'',
-        path:''
+export const hotList = [{
+        id: '1',
+        title: '纸巾湿品',
+        imgPath: '../../assets/img/1.png',
+        path: ''
     },
     {
-        id :'2',
-        title:'厨房烹饪',
-        imgPath:'',
-        path:''
+        id: '2',
+        title: '厨房烹饪',
+        imgPath: '',
+        path: ''
     },
     {
-        id :'3',
-        title:'日用百货',
-        imgPath:'',
-        path:''
+        id: '3',
+        title: '日用百货',
+        imgPath: '',
+        path: ''
     },
     {
-        id :'4',
-        title:'珠宝饰品',
-        imgPath:'',
-        path:''
+        id: '4',
+        title: '珠宝饰品',
+        imgPath: '',
+        path: ''
     },
     {
-        id :'5',
-        title:'收纳整理',
-        imgPath:'',
-        path:''
+        id: '5',
+        title: '收纳整理',
+        imgPath: '',
+        path: ''
     },
     {
-        id :'6',
-        title:'洗护清洁',
-        imgPath:'',
-        path:''
+        id: '6',
+        title: '洗护清洁',
+        imgPath: '',
+        path: ''
     },
     {
-        id :'7',
-        title:'清洁工具',
-        imgPath:'',
-        path:''
+        id: '7',
+        title: '清洁工具',
+        imgPath: '',
+        path: ''
     },
     {
-        id :'8',
-        title:'查看更多',
-        imgPath:'',
-        path:''
+        id: '8',
+        title: '查看更多',
+        imgPath: '',
+        path: ''
+    },
+    {
+        id: '1',
+        title: '纸巾湿品',
+        imgPath: '',
+        path: ''
+    },
+    {
+        id: '2',
+        title: '厨房烹饪',
+        imgPath: '',
+        path: ''
+    },
+    {
+        id: '3',
+        title: '日用百货',
+        imgPath: '',
+        path: ''
+    },
+    {
+        id: '4',
+        title: '珠宝饰品',
+        imgPath: '',
+        path: ''
+    },
+    {
+        id: '5',
+        title: '收纳整理',
+        imgPath: '',
+        path: ''
+    },
+    {
+        id: '6',
+        title: '洗护清洁',
+        imgPath: '',
+        path: ''
+    },
+    {
+        id: '7',
+        title: '清洁工具',
+        imgPath: '',
+        path: ''
+    },
+    {
+        id: '8',
+        title: '查看更多',
+        imgPath: '',
+        path: ''
     }
 ]
--- src/views/hot/Index.vue
+++ src/views/hot/Index.vue
@@ -1,8 +1,11 @@
 <template>
   <div>
-    <van-grid :gutter="0">
-      <van-grid-item v-for="item in hotList" :key="item.id" icon="photo-o" :text="item.title" />
-    </van-grid>
+    <div class="box">
+      <van-grid :gutter="0" :border="false">
+        <van-grid-item  v-for="item in hotList" :key="item.index" :icon="as" :text="item.title" />
+      </van-grid>
+    </div>
+
     <van-cell-group>
       <van-cell title="家居百货排行榜" value="高销量/好口碑" icon="location-o" />
     </van-cell-group>
@@ -22,7 +25,11 @@
       :price="item.price"
       :desc="item.desc"
       :title="item.title"
-      thumb="https://img.yzcdn.cn/vant/t-thirt.jpg" v-for="item in products" :key="item.id"  @click="productDetails(item.id)"/>
+      thumb="https://img.yzcdn.cn/vant/t-thirt.jpg"
+      v-for="item in products"
+      :key="item.id"
+      @click="productDetails(item.id)"
+    />
   </div>
 </template>
 
@@ -32,44 +39,72 @@ export default {
   data() {
     return {
       hotList,
-      products:[
+      // imgg:this.img,
+
+      products: [
         {
-          id:'1',
-          num:'1',
-          title:'男装',
-          desc:'男装描述信息',
-          price:'2.00',
-          thumb:'https://img.yzcdn.cn/vant/t-thirt.jpg'
+          id: "1",
+          num: "1",
+          title: "男装",
+          desc: "男装描述信息",
+          price: "2.00",
+          thumb: "https://img.yzcdn.cn/vant/t-thirt.jpg"
         },
-         {
-          id:'2',
-          num:'2',
-          title:'女装',
-          desc:'女装描述信息',
-          price:'2.00',
-          thumb:'https://img.yzcdn.cn/vant/t-thirt.jpg'
+        {
+          id: "2",
+          num: "2",
+          title: "女装",
+          desc: "女装描述信息",
+          price: "2.00",
+          thumb: "https://img.yzcdn.cn/vant/t-thirt.jpg"
         },
-         {
-          id:'3',
-          num:'3',
-          title:'童装装',
-          desc:'童装装描述信息',
-          price:'2.00',
-          thumb:'https://img.yzcdn.cn/vant/t-thirt.jpg'
-        },
+        {
+          id: "3",
+          num: "3",
+          title: "童装装",
+          desc: "童装装描述信息",
+          price: "2.00",
+          thumb: "https://img.yzcdn.cn/vant/t-thirt.jpg"
+        }
       ]
     };
   },
-  methods:{
-    productDetails(id) {
-      this.$router.push({path:'productDetails', query:{productid:id}});
+  methods: {
+    // id
+    productDetails() {
+      // this.$router.push({ path: "productDetails", query: { productid: id } });
+      console.log( this.img);
+    }
+  },
+  computed:{
+    img() {
+      let imgPath = [];
+      for(let i = 0; i < this.hotList.length; i++) {
+        imgPath.push(this.hotList[i].imgPath);
+      }
+      return imgPath;
     }
   }
-};  
+};
 </script>
 
 <style scoped>
 .van-cell {
   margin-top: 1em;
 }
+
+.van-grid-item::after {
+  border-width: 0px;
+  border: none
+}
+
+.box {
+  height: 170px;
+  overflow: scroll;
+}
+
+.box::-webkit-scrollbar {
+  display: none;
+}
+
 </style>
